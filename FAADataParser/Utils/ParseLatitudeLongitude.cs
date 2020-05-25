5137881788107a8b5887dc1c169d59eb5e41c8a1
Skand Hurkat <skandhurkat@gmail.com>
2020-05-25 16:17:25 -0700

    Add ParseLatitudeLongitude

---
+++ FAADataParser/Utils/ParseLatitudeLongitude.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FAADataParser.Utils
+{
+    static class ParseLatitudeLongitude
+    {
+        public static bool TryParse(string latLongString, out decimal latLong)
+        {
+            latLong = 0.0m;
+            Match matchLatitude = latitudeDegMinSecRegex.Match(latLongString);
+            Match matchLongitude = longitudeDegMinSecRegex.Match(latLongString);
+            Match matchAllSec = allSecRegex.Match(latLongString);
+            if (matchLatitude.Success || matchLongitude.Success)
+            {
+                Match match = matchLatitude.Success ? matchLatitude : matchLongitude;
+                if (!decimal.TryParse(match.Groups["Degrees"].Value, out decimal degrees))
+                {
+                    return false;
+                }
+                if (!decimal.TryParse(match.Groups["Minutes"].Value, out decimal minutes))
+                {
+                    return false;
+                }
+                if (!decimal.TryParse(match.Groups["Seconds"].Value, out decimal seconds))
+                {
+                    return false;
+                }
+                bool negative;
+                switch (match.Groups["Hemisphere"].Value)
+                {
+                    case "N": negative = false; break;
+                    case "S": negative = true; break;
+                    case "E": negative = false; break;
+                    case "W": negative = true; break;
+                    default: return false;
+                }
+                latLong = (negative ? -1.0m : 1.0m) * ((degrees * 3600m) + (minutes * 60m) + seconds);
+                return true;
+            }
+            else if (matchAllSec.Success)
+            {
+                Match match = matchAllSec;
+                if (!decimal.TryParse(match.Groups["Seconds"].Value, out decimal seconds))
+                {
+                    return false;
+                }
+                bool negative;
+                switch (match.Groups["Hemisphere"].Value)
+                {
+                    case "N": negative = false; break;
+                    case "S": negative = true; break;
+                    case "E": negative = false; break;
+                    case "W": negative = true; break;
+                    default: return false;
+                }
+                latLong = (negative ? -1.0m : 1.0m) * seconds;
+                return true;
+            }
+            return false;
+        }
+        private static readonly Regex latitudeDegMinSecRegex = new Regex(@"\b(?<Degrees>\d{2})-(?<Minutes>\d{2})-(?<Seconds>\d{2}\.\d{3,4})(?<Hemisphere>[NS])\b");
+        private static readonly Regex longitudeDegMinSecRegex = new Regex(@"\b(?<Degrees>\d{3})-(?<Minutes>\d{2})-(?<Seconds>\d{2}\.\d{3,4})(?<Hemisphere>[EW])\b");
+        private static readonly Regex allSecRegex = new Regex(@"\b(?<Seconds>\d{6}\.\d{3,4})(?<Hemisphere>[NSEW])\b");
+    }
+}
---
+++ FAADataParserTests/Utils/TestParseLatitudeLongitude.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+using FAADataParser.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AviationApp.UnitTests.FAADataParser.Utils
+{
+    [TestClass]
+    public class TestParseLatitudeLongitude
+    {
+        [TestMethod]
+        public void GoodLatitudeParsingTests()
+        {
+            List<(string, decimal)> dataList = new List<(string, decimal)>
+            {
+                (@"34-36-21.290N ", 124581.290m),
+                (@"47-38-18.000N ", 171498.000m),
+                (@"25-57-38.954S ", -93458.954m),
+                (@"25-57-38.9540S ", -93458.954m),
+                (@"140811.070N", 140811.070m),
+                (@"140811.0700N", 140811.070m),
+            };
+            foreach ((string, decimal) elem in dataList)
+            {
+                Assert.IsTrue(ParseLatitudeLongitude.TryParse(elem.Item1, out decimal result));
+                Assert.AreEqual(elem.Item2, result);
+            }
+        }
+        [TestMethod]
+        public void GoodLongitudeParsingTests()
+        {
+            List<(string, decimal)> dataList = new List<(string, decimal)>
+            {
+                (@"134-36-21.290E", 484581.290m),
+                (@"047-38-18.000E", 171498.000m),
+                (@"125-57-38.954W", -453458.954m),
+                (@"125-57-38.9540W", -453458.954m),
+                (@"271674.660W", -271674.660m),
+                (@"271674.6600W", -271674.660m)
+            };
+            foreach ((string, decimal) elem in dataList)
+            {
+                Assert.IsTrue(ParseLatitudeLongitude.TryParse(elem.Item1, out decimal result));
+                Assert.AreEqual(elem.Item2, result);
+            }
+        }
+    }
+}
