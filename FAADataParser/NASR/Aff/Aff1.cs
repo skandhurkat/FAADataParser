0ec0a090c73f949941bea64694b2860b90c1d900
Skand Hurkat <skandhurkat@gmail.com>
2020-05-25 17:55:21 -0700

    Break out Aff FacilityTypeParser into its own type

--- FAADataParser/NASR/Aff/Aff1.cs
+++ FAADataParser/NASR/Aff/Aff1.cs
@@ -4,14 +4,6 @@ using FAADataParser.Utils;
 
 namespace FAADataParser.NASR.Aff
 {
-    public enum FacilityType
-    {
-        AirRouteSurveillanceRadar,
-        AirRouteTrafficControlCentre,
-        CentreRadarApproachControlFacility,
-        RemoteCommunicationsAirGround,
-        SecondaryRadar
-    };
     class Aff1 : INASRDataParser
     {
         public string ArtccIdent { get; private set; }
@@ -37,7 +29,7 @@ namespace FAADataParser.NASR.Aff
             (8, 40, BuiltinTypeParsers.ParseString, nameof(ArtccName), false),
             (48, 30, BuiltinTypeParsers.ParseString, nameof(SiteLocation), false),
             (78, 50, BuiltinTypeParsers.ParseString, nameof(AltName), true),
-            (128, 5, ParseFacilityType, nameof(FacilityType), true),
+            (128, 5, FacilityTypeParser.ParseFacilityType, nameof(FacilityType), true),
             (133, 10, BuiltinTypeParsers.ParseDate, nameof(EffectiveDate), false),
             (143, 30, BuiltinTypeParsers.ParseString, nameof(StateName), false),
             (173, 2, BuiltinTypeParsers.ParseString, nameof(StateCode), false),
@@ -45,21 +37,6 @@ namespace FAADataParser.NASR.Aff
             (200, 14, ParseLatitudeLongitude.ParseLatLong, nameof(Longitude), true),
             (225, 4, BuiltinTypeParsers.ParseString, nameof(IcaoId), false)
         };
-
-        private static bool ParseFacilityType(string value, out object facilityType)
-        {
-            facilityType = null;
-            switch (value)
-            {
-                case "ARSR": facilityType = FacilityType.AirRouteSurveillanceRadar; break;
-                case "ARTCC": facilityType = FacilityType.AirRouteTrafficControlCentre; break;
-                case "CERAP": facilityType = FacilityType.CentreRadarApproachControlFacility; break;
-                case "RCAG": facilityType = FacilityType.RemoteCommunicationsAirGround; break;
-                case "SECRA": facilityType = FacilityType.SecondaryRadar; break;
-                default: return false;
-            }
-            return true;
-        }
 
         public bool Validate()
         {
---
+++ FAADataParser/NASR/Aff/FacilityTypeParser.cs
@@ -0,0 +1,29 @@
+﻿namespace FAADataParser.NASR.Aff
+{
+    public enum FacilityType
+    {
+        AirRouteSurveillanceRadar,
+        AirRouteTrafficControlCentre,
+        CentreRadarApproachControlFacility,
+        RemoteCommunicationsAirGround,
+        SecondaryRadar
+    };
+
+    static class FacilityTypeParser
+    {
+        public static bool ParseFacilityType(string value, out object facilityType)
+        {
+            facilityType = null;
+            switch (value)
+            {
+                case "ARSR": facilityType = FacilityType.AirRouteSurveillanceRadar; break;
+                case "ARTCC": facilityType = FacilityType.AirRouteTrafficControlCentre; break;
+                case "CERAP": facilityType = FacilityType.CentreRadarApproachControlFacility; break;
+                case "RCAG": facilityType = FacilityType.RemoteCommunicationsAirGround; break;
+                case "SECRA": facilityType = FacilityType.SecondaryRadar; break;
+                default: return false;
+            }
+            return true;
+        }
+    }
+}
