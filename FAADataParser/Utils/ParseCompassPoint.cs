aba8e39a63a6c5a95fa44eded84c17b008aa21eb
Skand Hurkat <skandhurkat@gmail.com>
2020-06-13 16:23:19 -0700

    Add ParseCompassPoint

---
+++ FAADataParser/Utils/ParseCompassPoint.cs
@@ -0,0 +1,49 @@
+﻿namespace FAADataParser.Utils
+{
+    public enum CompassPoint
+    {
+        North,
+        NorthNorthEast,
+        NorthEast,
+        EastNorthEast,
+        East,
+        EastSouthEast,
+        SouthEast,
+        SouthSouthEast,
+        South,
+        SouthSouthWest,
+        SouthWest,
+        WestSouthWest,
+        West,
+        WestNorthWest,
+        NorthWest,
+        NorthNorthWest
+    };
+    class ParseCompassPoint
+    {
+        public static bool TryParse(string input, out object compassPoint)
+        {
+            switch (input)
+            {
+                case "N": compassPoint = CompassPoint.North; break;
+                case "NNE": compassPoint = CompassPoint.NorthNorthEast; break;
+                case "NE": compassPoint = CompassPoint.NorthEast; break;
+                case "ENE": compassPoint = CompassPoint.EastNorthEast; break;
+                case "E": compassPoint = CompassPoint.East; break;
+                case "ESE": compassPoint = CompassPoint.EastSouthEast; break;
+                case "SE": compassPoint = CompassPoint.SouthEast; break;
+                case "SSE": compassPoint = CompassPoint.SouthSouthEast; break;
+                case "S": compassPoint = CompassPoint.South; break;
+                case "SSW": compassPoint = CompassPoint.SouthSouthWest; break;
+                case "SW": compassPoint = CompassPoint.SouthWest; break;
+                case "WSW": compassPoint = CompassPoint.WestSouthWest; break;
+                case "W": compassPoint = CompassPoint.West; break;
+                case "WNW": compassPoint = CompassPoint.WestNorthWest; break;
+                case "NW": compassPoint = CompassPoint.NorthWest; break;
+                case "NNW": compassPoint = CompassPoint.NorthNorthWest; break;
+                default: compassPoint = null; return false;
+            }
+            return true;
+        }
+    }
+}
