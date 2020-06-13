9b753b9109e1a3722c9f7cc47ab55446d022266b
Skand Hurkat <skandhurkat@gmail.com>
2020-06-13 16:40:47 -0700

    Add parser for Apt entries

---
+++ FAADataParser/NASR/Apt/Apt.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+using FAADataParser.Utils;
+
+namespace FAADataParser.NASR.Apt
+{
+    public enum LandingFacilityType
+    {
+        Airport,
+        Balloonport,
+        SeaplaneBase,
+        Gliderport,
+        Heliport,
+        Ultralight
+    };
+    public enum AirportOwnershipType
+    {
+        PubliclyOwned,
+        PrivatelyOwned,
+        AirforceOwned,
+        NavyOwned,
+        ArmyOwned,
+        CoastGuardOwned
+    };
+    public enum FacilityUse { Public, Private };
+    public enum SurveyMethod { Surveyed, Estimated };
+
+    class Apt : INASRDataParser
+    {
+        public string SiteNumber { get; private set; }
+        public LandingFacilityType LandingFacilityType { get; private set; }
+        public string LocationIdentifier { get; private set; }
+        public DateTime InformationEffectiveDate { get; private set; }
+        public string FAAFieldOffice { get; private set; }
+        public string StatePostOfficeCode { get; private set; }
+        public string StateName { get; private set; }
+        public string CountyName { get; private set; }
+        public string CountyStatePostOfficeCode { get; private set; }
+        public string CityName { get; private set; }
+        public string FacilityName { get; private set; }
+        public AirportOwnershipType AirportOwnershipType { get; private set; }
+        public FacilityUse FacilityUse { get; private set; }
+        public string OwnersName { get; private set; }
+        public string OwnersAddress1 { get; private set; }
+        public string OwnersAddress2 { get; private set; }
+        [Phone]
+        public string OwnersPhone { get; private set; }
+        public string ManagersName { get; private set; }
+        public string ManagersAddress1 { get; private set; }
+        public string ManagersAddress2 { get; private set; }
+        [Phone]
+        public string ManagersPhone { get; private set; }
+        public decimal Latitude { get; private set; }
+        public decimal Longitude { get; private set; }
+        public SurveyMethod ReferencePointDeterminationMethod { get; private set; }
+        public decimal Elevation { get; private set; }
+        public SurveyMethod ElevationDeterminationMethod { get; private set; }
+        public int MagneticVariation { get; private set; }
+        public int MagneticVariationEpochYear { get; private set; }
+        public int? TPA { get; private set; }
+        public string SectionalName { get; private set; }
+        public int DistanceFromCBDInNm { get; private set; }
+        public CompassPoint DirectionFromCBD { get; private set; }
+        public string BoundaryArtccIdentifier { get; private set; }
+        public string BoundaryArtccFaaComputerIdentifier { get; private set; }
+        public string BoundaryArtccName { get; private set; }
+        public string ResponsibleArtccIdentifier { get; private set; }
+        public string ResponsibleArtccFaaComputerIdentifier { get; private set; }
+        public string ResponsibleArtccName { get; private set; }
+        public bool TieInFssOnFacility { get; private set; }
+        public string TieInFssIdentifier { get; private set; }
+        public string TieInFssName { get; private set; }
+        [Phone]
+        public string LocalFssPhoneNumber { get; private set; }
+        [Phone]
+        public string TollFreeFssPhoneNumber { get; private set; }
+        public string AlternateFssIdentifier { get; private set; }
+        public string AlternateFssName { get; private set; }
+        [Phone]
+        public string TollFreeAlternateFssNumber { get; private set; }
+        public string NotamAndWeatherFacilityIdentifier { get; private set; }
+        public bool NotamDAvailableAtAirport { get; private set; }
+        public static bool TryParse(string recordString, out Apt apt)
+        {
+            return NASRDataParserGeneric<Apt>.TryParse(recordString, 1529, "APT", fieldList, out apt);
+        }
+
+        private static readonly List<(int fieldStart, int fieldLength, ParserDelegate parserDelegate, string propertyName, bool nullable)> fieldList = new List<(int fieldStart, int fieldLength, ParserDelegate parserDelegate, string propertyName, bool nullable)>
+        {
+            (3, 10, BuiltinTypeParsers.ParseString, nameof(SiteNumber), false),
+            (14, 13, LandingFacilityParser, nameof(LandingFacilityType), false),
+            (27, 4, BuiltinTypeParsers.ParseString, nameof(LocationIdentifier), false),
+            (31, 10,  BuiltinTypeParsers.ParseDate, nameof(InformationEffectiveDate), false),
+            (44, 4, BuiltinTypeParsers.ParseString, nameof(FAAFieldOffice), false),
+            (48, 2, BuiltinTypeParsers.ParseString, nameof(StatePostOfficeCode), false),
+            (50, 20, BuiltinTypeParsers.ParseString, nameof(StateName), false),
+            (70, 21, BuiltinTypeParsers.ParseString, nameof(CountyName), false),
+            (91, 2, BuiltinTypeParsers.ParseString, nameof(CountyStatePostOfficeCode), false),
+            (93, 40, BuiltinTypeParsers.ParseString, nameof(CityName), false),
+            (133, 50, BuiltinTypeParsers.ParseString, nameof(FacilityName), false),
+            (183, 2, AirportOwnershipTypeParser, nameof(AirportOwnershipType), false),
+            (185, 2, FacilityUseParser, nameof(FacilityUse), false),
+            (187, 35, BuiltinTypeParsers.ParseString, nameof(OwnersName), false),
+            (222, 72, BuiltinTypeParsers.ParseString, nameof(OwnersAddress1), false),
+            (294, 45, BuiltinTypeParsers.ParseString, nameof(OwnersAddress2), false),
+            (339, 16, ParsePhone.TryParse, nameof(OwnersPhone), false),
+            (355, 35, BuiltinTypeParsers.ParseString, nameof(ManagersName), false),
+            (390, 72, BuiltinTypeParsers.ParseString, nameof(ManagersAddress1), false),
+            (462, 45, BuiltinTypeParsers.ParseString, nameof(ManagersAddress2), false),
+            (507, 16, ParsePhone.TryParse, nameof(ManagersPhone), false),
+            (523, 15, ParseLatitudeLongitude.ParseLatLong, nameof(Latitude), false),
+            (550, 15, ParseLatitudeLongitude.ParseLatLong, nameof(Longitude), false),
+            (577, 1, SurveyMethodParser, nameof(ReferencePointDeterminationMethod), false),
+            (578, 7, BuiltinTypeParsers.ParseDecimal, nameof(Elevation), false),
+            (585, 1, SurveyMethodParser, nameof(ElevationDeterminationMethod), false),
+            (586, 3, MagVarParser, nameof(MagneticVariation), false),
+            (589, 4, BuiltinTypeParsers.ParseInt, nameof(MagneticVariationEpochYear), false),
+            (593, 4, BuiltinTypeParsers.ParseInt, nameof(TPA), true),
+            (597, 30, BuiltinTypeParsers.ParseString, nameof(SectionalName), false),
+            (627, 2, BuiltinTypeParsers.ParseInt, nameof(DistanceFromCBDInNm), false),
+            (629, 3, ParseCompassPoint.TryParse, nameof(DirectionFromCBD), false),
+            (637, 4, BuiltinTypeParsers.ParseString, nameof(BoundaryArtccIdentifier), false),
+            (641, 3, BuiltinTypeParsers.ParseString, nameof(BoundaryArtccFaaComputerIdentifier), false),
+            (644, 30, BuiltinTypeParsers.ParseString, nameof(BoundaryArtccName), false),
+            (674, 4, BuiltinTypeParsers.ParseString, nameof(ResponsibleArtccIdentifier), false),
+            (678, 3, BuiltinTypeParsers.ParseString, nameof(ResponsibleArtccFaaComputerIdentifier), false),
+            (681, 30, BuiltinTypeParsers.ParseString, nameof(ResponsibleArtccName), false),
+            (711, 1, BuiltinTypeParsers.ParseBool, nameof(TieInFssOnFacility), false),
+            (712, 4, BuiltinTypeParsers.ParseString, nameof(TieInFssIdentifier), false),
+            (716, 30, BuiltinTypeParsers.ParseString, nameof(TieInFssName), false),
+            (746, 16, ParsePhone.TryParse, nameof(LocalFssPhoneNumber), true),
+            (762, 16, ParsePhone.TryParse, nameof(TollFreeFssPhoneNumber), true),
+            (778, 4, BuiltinTypeParsers.ParseString, nameof(AlternateFssIdentifier), true),
+            (782, 30, BuiltinTypeParsers.ParseString, nameof(AlternateFssName), true),
+            (812, 16, ParsePhone.TryParse, nameof(TollFreeAlternateFssNumber), true),
+            (828, 4, BuiltinTypeParsers.ParseString, nameof(NotamAndWeatherFacilityIdentifier), false),
+            (832, 1, BuiltinTypeParsers.ParseBool, nameof(NotamDAvailableAtAirport), false)
+        };
+        public bool Validate() => true;
+
+        private static bool LandingFacilityParser(string val, out object landingFacilityType)
+        {
+            switch (val)
+            {
+                case "AIRPORT": landingFacilityType = LandingFacilityType.Airport; break;
+                case "BALLOONPORT": landingFacilityType = LandingFacilityType.Balloonport; break;
+                case "SEAPLANE BASE": landingFacilityType = LandingFacilityType.SeaplaneBase; break;
+                case "GLIDERPORT": landingFacilityType = LandingFacilityType.Gliderport; break;
+                case "HELIPORT": landingFacilityType = LandingFacilityType.Heliport; break;
+                case "ULTRALIGHT": landingFacilityType = LandingFacilityType.Ultralight; break;
+                default: landingFacilityType = null; return false;
+            }
+            return true;
+        }
+        private static bool AirportOwnershipTypeParser(string val, out object airportOwnershipType)
+        {
+            switch (val)
+            {
+                case "PU": airportOwnershipType = AirportOwnershipType.PubliclyOwned; break;
+                case "PR": airportOwnershipType = AirportOwnershipType.PrivatelyOwned; break;
+                case "MA": airportOwnershipType = AirportOwnershipType.AirforceOwned; break;
+                case "MN": airportOwnershipType = AirportOwnershipType.NavyOwned; break;
+                case "MR": airportOwnershipType = AirportOwnershipType.ArmyOwned; break;
+                case "CG": airportOwnershipType = AirportOwnershipType.CoastGuardOwned; break;
+                default: airportOwnershipType = null; return false;
+            }
+            return true;
+        }
+        private static bool FacilityUseParser(string val, out object facilityUse)
+        {
+            switch (val)
+            {
+                case "PU": facilityUse = FacilityUse.Public; break;
+                case "PR": facilityUse = FacilityUse.Private; break;
+                default: facilityUse = null; return false;
+            }
+            return true;
+        }
+        private static bool SurveyMethodParser(string val, out object surveyMethod)
+        {
+            switch (val)
+            {
+                case "E": surveyMethod = SurveyMethod.Estimated; break;
+                case "S": surveyMethod = SurveyMethod.Surveyed; break;
+                default: surveyMethod = null; return false;
+            }
+            return true;
+        }
+        private static bool MagVarParser(string val, out object magVar)
+        {
+            magVar = null;
+            Match match = magVarRegex.Match(val);
+            if (match.Success)
+            {
+                _ = BuiltinTypeParsers.ParseInt(match.Groups["Amount"].Value, out magVar);
+                if (match.Groups["Direction"].Value == "E")
+                {
+                    magVar = -(int)magVar;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private static readonly Regex magVarRegex = new Regex(@"\b(?<Amount>\d{2})(?<Direction>[EW])\b");
+    }
+}
---
+++ FAADataParserTests/NASR/Apt/TestApt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using FAADataParser.NASR.Apt;
+using FAADataParser.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FAADataParserTests.NASR.Apt
+{
+    [TestClass]
+    public class TestApt
+    {
+        [TestMethod]
+        public void TestParseKBFI()
+        {
+            string record = @"APT26396.*A   AIRPORT      BFI 04/23/2020ANMSEA WAWASHINGTON          KING                 WASEATTLE                                 BOEING FIELD/KING COUNTY INTL                     PUPUKING COUNTY                        516 THIRD AVE                                                           SEATTLE, WA 98104                              (206) 296-7380JOHN PARROTT, AAE                  7277 PERIMETER ROAD SOUTH                                               SEATTLE, WA 98108-3844                         (206) 296-733447-31-47.9000N 171107.9000N122-18-07.0000W440287.0000WE   21.6S15E2020    SEATTLE                       04S    634ZSE ZCSSEATTLE                       ZSE ZCSSEATTLE                       NSEA SEATTLE                                       1-800-WX-BRIEF                                                    BFI Y11/1938O IV A S 01/1974 NGPY3  NOT ANALYZED YNNYF F06262019        100LLA                                  MAJORMAJORHIGH/LOWHIGH/LOWSEE RMKSS-SR  Y122.950       N   CG Y 229040088026001000000010142      02791304953709450000117601/01/20193RD PARTY SURVEY02/02/20163RD PARTY SURVEY02/02/2016 HGR,TIE     AFRT,AMB,AVNCS,CARGO,CHTR,INSTR,RNTL,SALES,SURV                        Y-LKBFI   N                                                                                                                                                                                                                                                                                                                       ";
+            Assert.IsTrue(FAADataParser.NASR.Apt.Apt.TryParse(record, out FAADataParser.NASR.Apt.Apt apt));
+            Assert.AreEqual(LandingFacilityType.Airport, apt.LandingFacilityType);
+            Assert.AreEqual("BFI", apt.LocationIdentifier);
+            Assert.AreEqual(new DateTime(2020, 4, 23), apt.InformationEffectiveDate);
+            Assert.AreEqual("SEA", apt.FAAFieldOffice);
+            Assert.AreEqual("WA", apt.StatePostOfficeCode);
+            Assert.AreEqual("WASHINGTON", apt.StateName);
+            Assert.AreEqual("KING", apt.CountyName);
+            Assert.AreEqual("WA", apt.CountyStatePostOfficeCode);
+            Assert.AreEqual("SEATTLE", apt.CityName);
+            Assert.AreEqual("BOEING FIELD/KING COUNTY INTL", apt.FacilityName);
+            Assert.AreEqual(AirportOwnershipType.PubliclyOwned, apt.AirportOwnershipType);
+            Assert.AreEqual(FacilityUse.Public, apt.FacilityUse);
+            Assert.AreEqual("KING COUNTY", apt.OwnersName);
+            Assert.AreEqual("516 THIRD AVE", apt.OwnersAddress1);
+            Assert.AreEqual("SEATTLE, WA 98104", apt.OwnersAddress2);
+            Assert.AreEqual("+1 206-296-7380", apt.OwnersPhone);
+            Assert.AreEqual("JOHN PARROTT, AAE", apt.ManagersName);
+            Assert.AreEqual("7277 PERIMETER ROAD SOUTH", apt.ManagersAddress1);
+            Assert.AreEqual("SEATTLE, WA 98108-3844", apt.ManagersAddress2);
+            Assert.AreEqual("+1 206-296-7334", apt.ManagersPhone);
+            Assert.AreEqual(171107.9000m, apt.Latitude);
+            Assert.AreEqual(-440287.0000m, apt.Longitude);
+            Assert.AreEqual(SurveyMethod.Estimated, apt.ReferencePointDeterminationMethod);
+            Assert.AreEqual(21.6m, apt.Elevation);
+            Assert.AreEqual(SurveyMethod.Surveyed, apt.ElevationDeterminationMethod);
+            Assert.AreEqual(-15, apt.MagneticVariation);
+            Assert.AreEqual(2020, apt.MagneticVariationEpochYear);
+            Assert.IsNull(apt.TPA);
+            Assert.AreEqual("SEATTLE", apt.SectionalName);
+            Assert.AreEqual(04, apt.DistanceFromCBDInNm);
+            Assert.AreEqual(CompassPoint.South, apt.DirectionFromCBD);
+            Assert.AreEqual("ZSE", apt.BoundaryArtccIdentifier);
+            Assert.AreEqual("ZCS", apt.BoundaryArtccFaaComputerIdentifier);
+            Assert.AreEqual("SEATTLE", apt.BoundaryArtccName);
+            Assert.AreEqual("ZSE", apt.ResponsibleArtccIdentifier);
+            Assert.AreEqual("ZCS", apt.ResponsibleArtccFaaComputerIdentifier);
+            Assert.AreEqual("SEATTLE", apt.ResponsibleArtccName);
+            Assert.IsFalse(apt.TieInFssOnFacility);
+            Assert.AreEqual("SEA", apt.TieInFssIdentifier);
+            Assert.AreEqual("SEATTLE", apt.TieInFssName);
+            Assert.IsNull(apt.LocalFssPhoneNumber);
+            Assert.AreEqual("+1 800-992-7433", apt.TollFreeFssPhoneNumber);
+            Assert.IsNull(apt.AlternateFssIdentifier);
+            Assert.IsNull(apt.AlternateFssName);
+            Assert.IsNull(apt.TollFreeAlternateFssNumber);
+            Assert.AreEqual("BFI", apt.NotamAndWeatherFacilityIdentifier);
+            Assert.IsTrue(apt.NotamDAvailableAtAirport);
+        }
+    }
+}
