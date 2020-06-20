15612dae4b868f45593f7738d04ab4f6ecb3791a
Skand Hurkat <skandhurkat@gmail.com>
2020-06-20 12:17:11 -0700

    Stash Att record parser

---
+++ FAADataParser/NASR/Apt/Att.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using FAADataParser.Utils;
+
+namespace FAADataParser.NASR.Apt
+{
+    class Att : INASRDataParser
+    {
+        public string SiteNumber { get; private set; }
+        public int SequenceNumber { get; private set; }
+        public static bool TryParse(string recordString, out Att att)
+        {
+            return NASRDataParserGeneric<Att>.TryParse(recordString, 1529, "ATT", fieldList, out att);
+        }
+        private static readonly List<(int fieldStart, int fieldLength, ParserDelegate parserDelegate, string propertyName, bool nullable)> fieldList = new List<(int fieldStart, int fieldLength, ParserDelegate parserDelegate, string propertyName, bool nullable)>
+        {
+            (3, 11, BuiltinTypeParsers.ParseString, nameof(SiteNumber), false),
+            (16, 2, BuiltinTypeParsers.ParseInt, nameof(SequenceNumber), false),
+        };
+        public bool Validate() => true;
+    }
+}
