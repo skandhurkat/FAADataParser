8d1c2522ab081d2f684060cdceec3d897044ec01
Skand Hurkat <skandhurkat@gmail.com>
2020-08-02 15:34:14 -0700

    Placeholder Database class & project

---
+++ FAADatabase/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace FAADatabase
+{
+    public class Database
+    {
+        public Database() => InitializeAsync().SafeFireAndForget(false);
+
+        async Task InitializeAsync()
+        {
+            // Placeholder, just to shut up compiler warnings
+            _ = await Task.FromResult(0);
+        }
+
+        private static readonly SQLiteAsyncConnection _database = lazyInitializer.Value;
+        private static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() => new SQLiteAsyncConnection(DatabasePath, Flags));
+        private static string DatabasePath
+        {
+            get
+            {
+                string basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFileName);
+            }
+        }
+        private const string DatabaseFileName = "Database.db3";
+        private const SQLite.SQLiteOpenFlags Flags = SQLite.SQLiteOpenFlags.ReadWrite | SQLite.SQLiteOpenFlags.Create | SQLite.SQLiteOpenFlags.SharedCache;
+    }
+}
---
+++ FAADatabase/TaskExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FAADatabase
+{
+    /// <summary>
+    /// Extensions class copied from https://docs.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/data/databases.
+    /// Provides a way to safely call async methods in constructors and catch exceptions
+    /// </summary>
+    public static class TaskExtensions
+    {
+        // NOTE: Async void is intentional here. This provides a way
+        // to call an async method from the constructor while
+        // communicating intent to fire and forget, and allow
+        // handling of exceptions
+        public static async void SafeFireAndForget(this Task task,
+            bool returnToCallingContext,
+            Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+
+            // if the provided action is not null, catch and
+            // pass the thrown exception
+            catch (Exception ex) when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+}
