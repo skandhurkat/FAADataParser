43f380c750b843ab27969832e800fd223140c0a0
Skand Hurkat <skandhurkat@gmail.com>
2020-12-23 14:11:54 -0800

    Add cycles for 2021

--- FAADataParser/Cycle.cs
+++ FAADataParser/Cycle.cs
@@ -31,7 +31,14 @@ namespace FAADataParser
             (new DateTime(2020, 06, 18), false), (new DateTime (2020, 07, 16), true),
             (new DateTime(2020, 08, 13), false), (new DateTime (2020, 09, 10), true),
             (new DateTime(2020, 10, 08), false), (new DateTime (2020, 11, 05), true),
-            (new DateTime(2020, 12, 03), false), (new DateTime (2020, 12, 31), true)
+            (new DateTime(2020, 12, 03), false), (new DateTime (2020, 12, 31), true),
+            (new DateTime(2021, 01, 28), false), (new DateTime (2021, 02, 25), true),
+            (new DateTime(2021, 03, 25), false), (new DateTime (2021, 04, 22), true),
+            (new DateTime(2021, 05, 20), false), (new DateTime (2021, 06, 17), true),
+            (new DateTime(2021, 07, 15), false), (new DateTime (2021, 08, 12), true),
+            (new DateTime(2021, 09, 09), false), (new DateTime (2021, 10, 07), true),
+            (new DateTime(2021, 11, 04), false), (new DateTime (2021, 12, 02), true),
+            (new DateTime(2022, 12, 30), false), (new DateTime (2022, 01, 27), true)
         };
     }
 }
